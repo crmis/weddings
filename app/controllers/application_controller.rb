626ca2a400310ad61b8e8688ec6951fedadfd7d7
tom.jcox@icloud.com <tom.jcox@icloud.com>
2015-02-06 18:31:29 +0000

    Setup for controller auth and messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   # before_action :authenticate_user!
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to main_app.root_url, :alert => exception.message
+  end
 end
