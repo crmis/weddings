b09d025d05f8ff4ba5da1e9883e75e7b147d44d3
Tom Cox <tom.jcox@icloud.com>
2015-02-07 13:14:12 +0000

    Rmcats routing spec

---
+++ spec/routing/rmcats_routing_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+  RSpec.describe RmcatsController, :type => :routing do
+  describe "rmcats routing" do
+    it "routes to #index" do
+      expect(:get => "/rmcats").to route_to("rmcats#index")
+    end
+    it "routes to #new" do
+      expect(:get => "/rmcats/new").to route_to("rmcats#new")
+    end
+    it "routes to #show" do
+      expect(:get => "/rmcats/1").to route_to("rmcats#show", :id => "1")
+    end
+    it "routes to #edit" do
+      expect(:get => "/rmcats/1/edit").to route_to("rmcats#edit", :id => "1")
+    end
+    it "routes to #create" do
+      expect(:post => "/rmcats").to route_to("rmcats#create")
+    end
+    it "routes to #update" do
+      expect(:put => "/rmcats/1").to route_to("rmcats#update", :id => "1")
+    end
+    it "routes to #destroy" do
+      expect(:delete => "/rmcats/1").to route_to("rmcats#destroy", :id => "1")
+    end
+  end
+end
