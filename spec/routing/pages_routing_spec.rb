2dfce05e2af7b742515175718803464970e679a3
Tom Cox <tom.jcox@icloud.com>
2015-02-08 20:45:00 +0000

    Added pages routing spec

---
+++ spec/routing/pages_routing_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe PagesController, :type => :controller do
+  describe "GET #home" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :home
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "GET #contactus" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :contactus
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+  end
+end
