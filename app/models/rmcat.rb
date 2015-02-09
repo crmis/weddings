ec241e367c3812a01390fdddd2f9892afcd7da3c
Tom Cox <tom.jcox@icloud.com>
2015-02-09 14:41:09 +0000

    Fixed major issue, rmcats can be deleted if no rooms are assigned. Commit by @Koxzi95 and @mr-mitch

--- app/models/rmcat.rb
+++ app/models/rmcat.rb
@@ -6,12 +6,16 @@ class Rmcat < ActiveRecord::Base
     "#{id} #{rmtype}"
   end
 
-  before_destroy :rmcat_with_room?
+  def rmtype=(s)
+    super s.titleize
+  end
+
+  # before I am destroyed, check if rmcat is deletable
+  before_destroy :rmcat_deletable?
 
   private
-
-  def rmcat_with_room?
-    # Error message plz
+  def rmcat_deletable?
+    # the rmcat is deletable if it has no rooms assigned to it
     self.rooms.empty?
   end
 end
