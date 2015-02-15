b55ed606a034f83efbe51f67494bf5fddbab376f
Tom Cox <tom.jcox@icloud.com>
2015-02-15 01:00:48 +0000

    Strange, commit didn't work?

---
+++ spec/factories.rb
@@ -0,0 +1,37 @@
+# This will guess the User class
+FactoryGirl.define do
+  # factory :user do
+  #   first_name "John"
+  #   last_name  "Doe"
+  #   admin false
+  #   customer false
+  # end
+  #
+  # # Admin User
+  # factory :admin, class: User do
+  #   first_name "Admin"
+  #   last_name  "User"
+  #   admin true
+  #   customer false
+  # end
+  #
+  # # Customer User
+  # factory :customer, class: User do
+  #   first_name "Admin"
+  #   last_name  "User"
+  #   admin false
+  #   customer true
+  # end
+
+  # :name, :description, :size, :capacity, :price, :picture, :rmcat_id
+  factory :room do
+    name "The New Room"
+    description  "The Really Good New Room"
+    size "250"
+    capacity "650"
+    price "6000"
+    picture { File.new("#{Rails.root}/spec/support/fixtures/image.jpg") }
+  end
+
+
+end
