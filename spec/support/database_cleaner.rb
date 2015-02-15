7000371491d7fe6bb6e035500c1e9297c28fd347
Tom Cox <tom.jcox@icloud.com>
2015-02-15 16:16:40 +0000

    Getting somewhere with database cleaner, its actually a FileUtil I need not the cleaner/linter

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,18 +1,22 @@
-# RSpec.configure do |config|
-#   # Database cleaner config before and after testing
-#   config.before(:suite) do
-#     DatabaseCleaner.clean_with(:truncation)
-#   end
-#
-#   config.before(:each) do
-#     DatabaseCleaner.strategy = :transaction
-#   end
-#
-#   config.before(:each) do
-#     DatabaseCleaner.start
-#   end
-#
-#   config.after(:each) do
-#     DatabaseCleaner.clean
-#   end
-# end
+RSpec.configure do |config|
+  # Database cleaner config before and after testing
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+  config.after(:each) do
+    FileUtils.rm @file_path
+  end
+end
