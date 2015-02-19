bae42060b23eac3639c95a540da0235affac1679
tom.jcox@icloud.com <tom.jcox@icloud.com>
2015-02-19 16:12:08 +0000

    Removed these

--- app/models/room.rb
+++ app/models/room.rb
@@ -8,18 +8,18 @@ class Room < ActiveRecord::Base
 
   validates :name, :presence => { :message => "cannot be blank ..."}
 	validates :name, :length => {:in => 2..35, :message => "must be between two and thirty five characters"}
-  #
+
   validates :description, :presence => { :message => "cannot be blank ..."}
 	validates :description, :length => {:maximum => 2000, :message => "maximum length 2000 characters"}
-  #
+
   validates :size, :presence => { :message => "cannot be blank ..."}
 	validates :size, :numericality => {:greater_than_or_equal_to => 10, :message => "Size: cannot be less than ten"}
   validates :size, :numericality => {:less_than => 2001, :message => "Price: cannot more than 2000"}
-  #
+
   validates :capacity, :presence => { :message => "cannot be blank ..."}
   validates :capacity, :numericality => {:greater_than_or_equal_to => 10, :message => "Size: cannot be less than ten"}
   validates :capacity, :numericality => {:less_than => 1001, :message => "Price: cannot more than 1000"}
-  #
+
   validates :price, :presence => { :message => "cannot be blank ..."}
   validates :price, :numericality => {:greater_than_or_equal_to => 100, :message => "Price: cannot be less than 100"}
   validates :price, :numericality => {:less_than => 5001, :message => "Price: cannot more than 5000"}
