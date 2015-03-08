50556f9f8ddb980e237d1183dfb36cad03f90f29
Tom Cox <tom.jcox@icloud.com>
2015-03-08 17:28:48 +0000

    Grunt files now updated

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,46 @@
+// grunt -v (verbose output)
+/*global module:false*/
 module.exports = function(grunt) {
-
 	// Project configuration.
 	grunt.initConfig({
+		// Metadata.
 		pkg: grunt.file.readJSON('package.json'),
+		datetime: Date.now(),
 		uglify: {
 			options: {
-				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+				mangle: {
+					toplevel: true
+				},
+				squeeze: {
+					dead_code: false
+				},
+				codegen: {
+					quote_keys: true
+				}
 			},
-			build: {
-				src: 'src/<%= pkg.name %>.js',
-				dest: 'build/<%= pkg.name %>.min.js'
+			'weddings': {
+				src: 'app/assets/javascripts/custom.js',
+				dest: 'build/testfileoutput.min.js'
+			}
+		},
+		//cssmin: {
+		//'weddings': {
+		//files: {
+		//'build/cssoutput.min.css': ['src/css/file1.css', 'src/css/file2.css']
+		//}
+		//}
+		//},
+		'check-gems': {
+			test: {
+				files: [{
+					src: ['./', 'test']
+				}]
 			}
 		}
 	});
-
-	// Load the plugin that provides the "uglify" task.
 	grunt.loadNpmTasks('grunt-contrib-uglify');
-
-	// Default task(s).
-	grunt.registerTask('default', ['uglify']);
-
+	//grunt.loadNpmTasks('grunt-contrib-cssmin'); 'cssmin' in tasks
+	grunt.loadNpmTasks('grunt-check-gems');
+	// Default task. //:weddings
+	grunt.registerTask('default', ['uglify', 'check-gems']);
 };
