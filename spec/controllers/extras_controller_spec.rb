85b13b986a60056e593ecd2e1f5484e408f56a8f
Tom Cox <tom.jcox@icloud.com>
2015-03-12 21:48:54 +0000

    Extras tested

--- spec/controllers/extras_controller_spec.rb
+++ spec/controllers/extras_controller_spec.rb
@@ -4,19 +4,19 @@ require 'rails_helper'
 # General note for all tests, because I added 'config.include FactoryGirl::Syntax::Methods'
 # => to spec/rails_helper.rb, you no longer have to call FactoryGirl before commands such as 'create'
 
-RSpec.describe RoomsController, :type => :controller do
+RSpec.describe ExtrasController, :type => :controller do
 
-	# bundle exec rspec spec\controllers\rooms_controller_spec.rb
-	
+	# bundle exec rspec spec\controllers\extras_controller_spec.rb
+
 	# def valid_session
 	#   sign_in :user, @admin
 	# end
 
 	describe "GET #index" do
-		it "populates an array of rooms" do
-			room = create(:room)
+		it "populates an array of extras" do
+			extra = create(:extra)
 			get :index
-			assigns(:rooms).should eq([room])
+			assigns(:extras).should eq([extra])
 		end
 
 		it "renders the :index view" do
@@ -26,14 +26,14 @@ RSpec.describe RoomsController, :type => :controller do
 	end
 
 	describe "GET #show" do
-		it "assigns the requested room to @room" do
-			room = create(:room)
-			get :show, id: room
-			assigns(:room).should eq(room)
+		it "assigns the requested extra to @extra" do
+			extra = create(:extra)
+			get :show, id: extra
+			assigns(:extra).should eq(extra)
 		end
 
 		it "renders the #show view" do
-			get :show, id: create(:room)
+			get :show, id: create(:extra)
 			response.should render_template :show
 		end
 	end
@@ -42,12 +42,12 @@ RSpec.describe RoomsController, :type => :controller do
 	before { subject.stub(current_user: admin, authenticate_user!: true) }
 	describe "POST create" do
 		context "with valid attributes" do
-			it "creates a new room" do
-				expect{ post :create, room: attributes_for(:room) }#.to change(Room,:count).by(1)
+			it "creates a new extra" do
+				expect{ post :create, extra: attributes_for(:extra) }#.to change(Room,:count).by(1)
 			end
 
-			it "redirects to the new room" do
-				post :create, room: attributes_for(:room)
+			it "redirects to the new extra" do
+				post :create, extra: attributes_for(:extra)
 				response.should render_template :new
 			end
 		end
