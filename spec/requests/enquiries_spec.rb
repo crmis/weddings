3a2374ba14999265b088fbabbce566b79e49996e
Tom Cox <tom.jcox@icloud.com>
2015-03-21 02:27:38 +0000

    Specs correct

--- spec/requests/enquiries_spec.rb
+++ spec/requests/enquiries_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Enquiries", type: :request do
-  describe "GET /enquiries" do
-    it "works! (now write some real specs)" do
-      get enquiries_path
-      expect(response).to have_http_status(200)
-    end
-  end
+	describe "GET /enquiries" do
+		context "with no user" do
+			it "gets index for enquiries" do
+				get enquiries_path
+				expect(response).to have_http_status(302)
+			end
+		end
+	end
 end
