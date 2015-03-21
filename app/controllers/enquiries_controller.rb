2c2087068cfbfd6bfaa1556aa6ce74e2a1f74bc1
Tom Cox <tom.jcox@icloud.com>
2015-03-21 02:06:47 +0000

    Only admin can manage

--- app/controllers/enquiries_controller.rb
+++ app/controllers/enquiries_controller.rb
@@ -1,6 +1,6 @@
 class EnquiriesController < ApplicationController
   before_action :set_enquiry, only: [:show, :edit, :update, :destroy]
-
+	load_and_authorize_resource
   # GET /enquiries
   def index
     @enquiries = Enquiry.all
