3750ea9b3f0066f29b6367cd40217369c11be2d2
Tom Cox <tom.jcox@icloud.com>
2015-03-22 21:43:33 +0000

    Rooms request spec

---
+++ spec/requests/rooms_spec.rb
@@ -0,0 +1,13 @@
+# @author Tom Cox <https://github.com/koxzi95>
+require 'rails_helper'
+# rspec spec\requests\rooms_spec.rb
+RSpec.describe "Rooms", type: :request do
+	describe "GET /rooms" do
+		context "with normal browsing" do
+			it "gets index for rooms" do
+				get rooms_path
+				expect(response).to have_http_status(200)
+			end
+		end
+	end
+end
