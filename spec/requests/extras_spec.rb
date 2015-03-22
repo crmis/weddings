964b6d21e948fb9d3817c0e48ac810f9d11fd37d
Tom Cox <tom.jcox@icloud.com>
2015-03-22 21:39:09 +0000

    Extras request spec

---
+++ spec/requests/extras_spec.rb
@@ -0,0 +1,13 @@
+# @author Tom Cox <https://github.com/koxzi95>
+require 'rails_helper'
+# rspec spec\requests\extras_spec.rb
+RSpec.describe "Extras", type: :request do
+	describe "GET /enquiries" do
+		context "with normal browsing" do
+			it "gets index for extras" do
+				get extras_path
+				expect(response).to have_http_status(200)
+			end
+		end
+	end
+end
