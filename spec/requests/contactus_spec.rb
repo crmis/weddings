294ad1bdef8498e9613bc28466a4874101596e22
Tom Cox <tom.jcox@icloud.com>
2015-03-23 21:36:37 +0000

    Request specs done

--- spec/requests/contactus_spec.rb
+++ spec/requests/contactus_spec.rb
@@ -1,10 +1,11 @@
 # @author Tom Cox <https://github.com/koxzi95>
 require "rails_helper"
-
-RSpec.describe PagesController, :type => :controller do
-	describe "GET #home" do
+# rspec spec\requests\contactus_spec.rb
+RSpec.describe "Contact Us", type: :request do
+	describe "GET #contactus" do
+		context "with normal browsing"
 		it "responds successfully with an HTTP 200 status code" do
-			get :home
+			get '/pages/contactus'
 			expect(response).to be_success
 			expect(response).to have_http_status(200)
 		end
