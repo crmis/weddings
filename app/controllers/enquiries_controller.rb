ff43fb9eade20a17aa24fb721973deb1470e702e
Tom Cox <tom.jcox@icloud.com>
2015-03-24 22:04:50 +0000

    Ran beautifier

--- app/controllers/enquiries_controller.rb
+++ app/controllers/enquiries_controller.rb
@@ -8,68 +8,68 @@ class EnquiriesController < ApplicationController
 	# The skip is so users can view the rooms but nothing else.
 	load_and_authorize_resource
 
-  # GET /enquiries
-  def index
-    @enquiries = Enquiry.all
-  end
+	# GET /enquiries
+	def index
+		@enquiries = Enquiry.all
+	end
 
-  # GET /enquiries/1
-  def show
-  end
+	# GET /enquiries/1
+	def show
+	end
 
-  # GET /enquiries/new
-  def new
-    @enquiry = Enquiry.new
+	# GET /enquiries/new
+	def new
+		@enquiry = Enquiry.new
 		render 'pages/contactus'
-  end
+	end
 
-  # GET /enquiries/1/edit
-  def edit
-  end
+	# GET /enquiries/1/edit
+	def edit
+	end
 
-  # POST /enquiries
-  def create
-    @enquiry = Enquiry.new(enquiry_params)
-			if current_user.customer?
-				@enquiry.user_id = current_user.id
-			end
-    if @enquiry.save
-      redirect_to '/pages/contactus', notice: 'Enquiry was successfully sent.'
-    else
-      render :new
-    end
-  end
-
-  # PATCH/PUT /enquiries/1
-  def update
-    if @enquiry.update(enquiry_params)
-      redirect_to @enquiry, notice: 'Enquiry was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  # DELETE /enquiries/1
-  def destroy
-    @enquiry.destroy
-    redirect_to enquiries_url, notice: 'Enquiry was successfully destroyed.'
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_enquiry
-      @enquiry = Enquiry.find(params[:id])
-    end
-
-		# If resource not found redirect to root and flash error.
-		def resource_not_found
-			yield
-		rescue ActiveRecord::RecordNotFound
-			redirect_to root_url, :notice => "Room not found."
+	# POST /enquiries
+	def create
+		@enquiry = Enquiry.new(enquiry_params)
+		if current_user.customer?
+			@enquiry.user_id = current_user.id
 		end
+		if @enquiry.save
+			redirect_to '/pages/contactus', notice: 'Enquiry was successfully sent.'
+		else
+			render :new
+		end
+	end
 
-    # Only allow a trusted parameter "white list" through.
-    def enquiry_params
-      params.require(:enquiry).permit(:subject, :e_description, :user_id)
-    end
+	# PATCH/PUT /enquiries/1
+	def update
+		if @enquiry.update(enquiry_params)
+			redirect_to @enquiry, notice: 'Enquiry was successfully updated.'
+		else
+			render :edit
+		end
+	end
+
+	# DELETE /enquiries/1
+	def destroy
+		@enquiry.destroy
+		redirect_to enquiries_url, notice: 'Enquiry was successfully destroyed.'
+	end
+
+	private
+	# Use callbacks to share common setup or constraints between actions.
+	def set_enquiry
+		@enquiry = Enquiry.find(params[:id])
+	end
+
+	# If resource not found redirect to root and flash error.
+	def resource_not_found
+		yield
+	rescue ActiveRecord::RecordNotFound
+		redirect_to root_url, :notice => "Room not found."
+	end
+
+	# Only allow a trusted parameter "white list" through.
+	def enquiry_params
+		params.require(:enquiry).permit(:subject, :e_description, :user_id)
+	end
 end
--- app/models/extracat.rb
+++ app/models/extracat.rb
@@ -1,23 +1,23 @@
 # @author Richard Mitchell <https://github.com/mr-mitch>
 # @author Stacey Rees <https://github.com/staceysmells>
 class Extracat < ActiveRecord::Base
-  has_many :extras#
-  def extracat_info
-    "#{id} #{extratype}"
-  end
+	has_many :extras#
+	def extracat_info
+		"#{id} #{extratype}"
+	end
 
-  def extratype=(s)
-    super s.titleize
-  end
+	def extratype=(s)
+		super s.titleize
+	end
 
 	validates :extratype, :presence => { :message => "cannot be blank."}
 
-  # before I am destroyed, check if rmcat is deletable
-  before_destroy :extracat_deletable?
+	# before I am destroyed, check if rmcat is deletable
+	before_destroy :extracat_deletable?
 
-  private
-  def extracat_deletable?
-    # the rmcat is deletable if it has no rooms assigned to it
-    self.extras.empty?
-  end
+	private
+	def extracat_deletable?
+		# the rmcat is deletable if it has no rooms assigned to it
+		self.extras.empty?
+	end
 end
