b8844659df72343921f205836446e693a846b1fb
Tom Cox <tom.jcox@icloud.com>
2015-03-25 23:12:33 +0000

    New asset precompile

---
+++ spec/views/enquiries/show.html.erb_spec.rb
@@ -0,0 +1,18 @@
+# @author Tom Cox <https://github.com/koxzi95>
+require 'rails_helper'
+# rspec spec\views\enquiries\show.html.erb_spec.rb
+# TODO Need to fix 'customer_name' error
+# RSpec.describe "enquiries/show", type: :view do
+# 	before(:each) do
+# 		@enquiry = assign(:enquiry, Enquiry.create!(
+# 			:subject => "Subject",
+# 			:e_description => "MyText"
+# 		))
+# 	end
+#
+# 	it "renders attributes in <p>" do
+# 		render
+# 		expect(rendered).to match(/Subject/)
+# 		expect(rendered).to match(/MyText/)
+# 	end
+# end
