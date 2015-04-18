df68348397c00b15792f3eb01f27ed5a839e58dd
Tom Cox <tom.jcox@icloud.com>
2015-04-18 15:34:52 +0100

    Done associations, first table might be obselete

--- app/models/addition.rb
+++ app/models/addition.rb
@@ -1,2 +1,4 @@
 class Addition < ActiveRecord::Base
+  belongs_to :booking
+  belongs_to :extra
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 require_relative './concerns/bookable'
 class Booking < ActiveRecord::Base
   include Bookable
+  has_many :additions
+  has_many :extras, :through => :additions
 end
