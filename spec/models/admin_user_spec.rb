133ebd9abd7a792b796ef64dfc942a96a430fbeb
Tom Cox <tom.jcox@icloud.com>
2015-04-20 11:07:32 +0100

    Updated admin user spec

--- spec/models/admin_user_spec.rb
+++ spec/models/admin_user_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe AdminUser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    expect(create(:admin_user)).to be_valid
+  end
 end
